abf476046fe84e3b26be14e7f6fc18af5edd7bc1
Héctor López <hector.lopez@DSA-DEV2-10>
2019-12-19 12:39:43 -0600

    channel with join

--- nairobi/lib/nairobi_web/channels/producto_channel.ex
+++ nairobi/lib/nairobi_web/channels/producto_channel.ex
@@ -1,5 +1,8 @@
-defmodule Nairobi.ProductoChannel do
+defmodule NairobiWeb.ProductoChannel do
+  use NairobiWeb, :channel
 
-
+  def join("producto:" <> clave_producto, _params, socket) do
+    {:ok, assign(socket, :clave_producto, clave_producto)}
+  end
 
 end
--- nairobi/lib/nairobi_web/channels/user_socket.ex
+++ nairobi/lib/nairobi_web/channels/user_socket.ex
@@ -3,6 +3,7 @@ defmodule NairobiWeb.UserSocket do
 
   ## Channels
   # channel "room:*", NairobiWeb.RoomChannel
+  channel("producto", NairobiWeb.ProductoChannel)
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
